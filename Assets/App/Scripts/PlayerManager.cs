f7c73f83233aa4e9f38038f87203161fb304fc6e
zundou <h240503@gmail.com>
2023-04-29 14:53:32 +0900

    feat: 宇宙っぽい移動処理

--- Assets/App/Scripts/PlayerManager.cs
+++ Assets/App/Scripts/PlayerManager.cs
@@ -4,36 +4,33 @@ namespace App.Scripts
 {
     public class PlayerManager : MonoBehaviour
     {
-        private float speed = 0.01f;
-        
+        private float thrust = 5.0f;
+        private float maxSpeed = 5.0f;
+        private float drag = 0.25f;
+
+        private Rigidbody2D rb;
+
         // Start is called before the first frame update
         void Start()
         {
+            rb = GetComponent<Rigidbody2D>();
         }
-        // Update is called once per frame
-        void Update()
+
+        // FixedUpdate is called once per fixed time interval
+        void FixedUpdate()
         {
             // プレイヤーは無重力の空間に対して上下左右キーを入力することで移動ができる
-            Vector2 position = transform.position;
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
 
-            if (Input.GetKey("left"))
-            {
-                position.x -= speed;
-            }
-            if (Input.GetKey("right"))
-            {
-                position.x += speed;
-            }
-            if (Input.GetKey("up"))
-            {
-                position.y += speed;
-            }
-            if (Input.GetKey("down"))
-            {
-                position.y -= speed;
-            }
+            Vector2 force = new Vector2(horizontal * thrust, vertical * thrust);
+            rb.AddForce(force);
 
-            transform.position = position;
+            // Limit the player's speed
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+
+            // Apply drag force
+            rb.velocity -= rb.velocity * drag * Time.fixedDeltaTime;
         }
     }
 }
