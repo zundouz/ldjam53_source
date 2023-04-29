d3064d0e462f6ec0b652b8343498b6dde7a238c2
zundou <h240503@gmail.com>
2023-04-29 16:29:27 +0900

    feat: SobaBoxの作成と、ぶつかったときに個別に判定取る
    - （あとでPrefab化する…？でもそれだと個別ID取得が面倒かも…？）

---
+++ Assets/App/Scripts/SobaBoxManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace App.Scripts
+{
+    public class SobaBoxManager : MonoBehaviour
+    {
+        [SerializeField] private int SobaBoxId; // ID を設定することで、どのそば箱がぶつかったかを判別できるようにする
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+        
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.collider.CompareTag($"Wall"))
+            {
+                Debug.Log($"ぶつかりました。ID: {SobaBoxId}");
+            }
+        }
+    }
+}
