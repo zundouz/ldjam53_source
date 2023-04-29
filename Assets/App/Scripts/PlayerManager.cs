68927058c31b69bcb6dd9bdeb787d7e5e02cf170
zundou <h240503@gmail.com>
2023-04-29 14:45:26 +0900

    feat: プレイヤー移動

--- Assets/App/Scripts/PlayerManager.cs
+++ Assets/App/Scripts/PlayerManager.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour
+namespace App.Scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerManager : MonoBehaviour
     {
+        private float speed = 0.01f;
         
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            // プレイヤーは無重力の空間に対して上下左右キーを入力することで移動ができる
+            Vector2 position = transform.position;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            if (Input.GetKey("left"))
+            {
+                position.x -= speed;
+            }
+            if (Input.GetKey("right"))
+            {
+                position.x += speed;
+            }
+            if (Input.GetKey("up"))
+            {
+                position.y += speed;
+            }
+            if (Input.GetKey("down"))
+            {
+                position.y -= speed;
+            }
+
+            transform.position = position;
+        }
     }
 }
