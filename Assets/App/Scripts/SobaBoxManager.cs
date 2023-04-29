49b89f8147a42f2667d50b17479e199ae3826e7b
zundou <h240503@gmail.com>
2023-04-29 19:35:11 +0900

    feat: 中の箱が衝突してしまったら、それより上もすべて衝突判定としてしまう
    - これで「上から詰める処理」を大幅簡素化できた

--- Assets/App/Scripts/SobaBoxManager.cs
+++ Assets/App/Scripts/SobaBoxManager.cs
@@ -2,8 +2,32 @@ using UnityEngine;
 
 namespace App.Scripts
 {
+
+    
     public class SobaBoxManager : MonoBehaviour
     {
+        public enum SobaBoxHavingState
+        {
+            OnPlayer,
+            AwayFromPlayer,
+            Destroyed
+        };
+
+        // 最初のステート
+        public static SobaBoxHavingState[] SobaBoxHavingStateList =
+        {
+            SobaBoxHavingState.OnPlayer,
+            SobaBoxHavingState.OnPlayer,
+            SobaBoxHavingState.OnPlayer,
+            SobaBoxHavingState.OnPlayer,
+            SobaBoxHavingState.OnPlayer,
+            SobaBoxHavingState.OnPlayer,
+            SobaBoxHavingState.OnPlayer,
+            SobaBoxHavingState.OnPlayer,
+            SobaBoxHavingState.OnPlayer,
+            SobaBoxHavingState.OnPlayer,
+        };
+        
         [SerializeField] private int SobaBoxId; // ID を設定することで、どのそば箱がぶつかったかを判別できるようにする
         // [SerializeField] private PhysicsMaterial2D noFrictionMaterial; // できないらしい
         
@@ -19,12 +43,19 @@ namespace App.Scripts
         // Update is called once per frame
         void Update()
         {
-        
+            // 自分より上のそば箱を全て無効にする
+            if (SobaBoxHavingStateList[SobaBoxId] == SobaBoxHavingState.AwayFromPlayer)
+            {
+                for (int i = SobaBoxId; i < SobaBoxHavingStateList.Length; i++)
+                {
+                    SobaBoxHavingStateList[i] = SobaBoxHavingState.AwayFromPlayer;
+                }
+            }
         }
         
         void OnCollisionEnter2D(Collision2D collision)
         {
-            if (collision.collider.CompareTag($"Wall"))
+            if (collision.collider.CompareTag($"Wall") || SobaBoxHavingStateList[SobaBoxId] == SobaBoxHavingState.AwayFromPlayer)
             {
                 Debug.Log($"ぶつかりました。ID: {SobaBoxId}");
                 
@@ -36,9 +67,23 @@ namespace App.Scripts
                 {
                     // RigidBody2Dコンポーネントを、新規でアタッチ
                     var rb = gameObject.AddComponent<Rigidbody2D>();
-                    rb.gravityScale = 0.1f;
+                    rb.gravityScale = 0.01f;
+                    rb.angularVelocity = 0.05f;
                     _isAwayFromPlayer = true;
                     gameObject.transform.parent = null;
+                    
+                    // 色を変えて、壁に当たったことを分かりやすくする
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color(0.35f, 0.35f, 0.35f, 1.0f);
+                    // // BoxColliderを取得し、Colliderの当たり判定を無効化する
+                    // gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                    
+                    SobaBoxHavingStateList[SobaBoxId] = SobaBoxHavingState.AwayFromPlayer;
+                    
+                    // // SobaBoxHavingStateList の中身をデバッグ表示
+                    // foreach (var state in SobaBoxHavingStateList)
+                    // {
+                    //     Debug.Log($"{state}");
+                    // }
                 }
             }
         }
